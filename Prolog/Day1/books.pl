47eb4b7ba0d528b614de65df949ded16c1cd1455
James Kirk <james.kirk84@gmail.com>
2013-11-11 12:49:53 +0000

    Prolog Day 1

---
+++ Prolog/Day1/books.pl
@@ -0,0 +1,3 @@
+book(dirk_gentlys_holistic_detective_agency, douglas_adams).
+book(hitchhikers_guide_to_the_galaxy, douglas_adams).
+book(seven_languages_in_seven_weeks, bruce_tate).
---
+++ Prolog/Day1/music.pl
@@ -0,0 +1,9 @@
+musician(jimi_hendrix, guitar).
+musician(eric_clapton, guitar).
+musician(rodriguez, guitar).
+musician(john_bonham, drums).
+
+genre(jimi_hendrix, rock).
+genre(eric_clapton, rock).
+genre(rodriguez, folk).
+genre(john_bonham, rock).
