60b7c29248a9a3ceb8ce13fc9b9599e4ff972328
James Kirk <james.kirk84@gmail.com>
2013-12-01 20:51:27 +0000

    Erlang Day1 exercise 1

---
+++ Erlang/Day1/words.erl
@@ -0,0 +1,11 @@
+% a function that uses recursion to return the number of words in a string (string is treated as a list of chars)
+-module(words).
+-export([count/1]).
+
+count_core(32) -> 1; % match space (32 = ASCII Space)
+count_core([Head | Tail]) -> count_core(Head) + count_core(Tail); % recursive call
+count_core(_) -> 0.  % any other character
+
+count([]) -> 0; %special case for empty string
+count(List) -> count_core(List) + 1.
+
