6c9d53f191ad793105042776ec3233f7acd5b810
James Kirk <james.kirk84@gmail.com>
2013-12-01 21:06:01 +0000

    Erlang Day1 exercise 2

---
+++ Erlang/Day1/count_to_ten.erl
@@ -0,0 +1,10 @@
+% Write a function that uses recursion to count to ten
+-module(count_to_ten).
+-export([count_to_ten/0]).
+
+recursive_count(10) -> [10]; %termination case
+recursive_count(Count) -> [Count] ++ recursive_count(Count + 1). %recursive call
+
+count_to_ten() -> recursive_count(1).
+
+
